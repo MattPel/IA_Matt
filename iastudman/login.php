eb19cf4cf326af003bd084e80efbd0867f9a366c
Matheos Pelekis <matheos.p@gmail.com>
2019-01-24 15:54:03 +0100

    commit message

---
+++ iastudman/login.php
@@ -0,0 +1,51 @@
+<?php
+	if ($_SERVER['REQUEST_METHOD'] == 'POST'){
+		
+		$servername = "localhost";
+		$username = "root";
+		$password = "";
+		$dbname = "German_Class";
+		
+		//establish the connection with database and server credentials
+		$conn = new mysqli($servername, $username, $password, $dbname);
+		
+		//if there is an error during connection do the following
+		if ($conn->connect_error) {
+			http_response_code(500);
+            echo "<br> <br> <p id='zeroResults' style='text-align: center; font-size: 130%; color: #0d3800; font-weight: bold;'>
+			Connection failed: " . $conn->connect_error . "</p>";
+			die("Connection failed: " . $conn->connect_error);
+		} 
+		//if there is no error during connection do the following
+		else{
+			$sql = "SELECT Username FROM credentials WHERE Username='" . $_POST['uname'] . "' AND Password='" . $_POST['psw'] . "'";
+			
+			$result = $conn->query($sql);
+			
+			//if the input matches the database record do the following
+			if ($result){
+				if ($result->num_rows>0){
+					$row = $result->fetch_assoc();
+					echo $row['Username'];
+				}
+				//if the input does not match the database record do the following
+				else {
+					echo "Error during login: Username and password do not match, please try again!";
+				}
+			}
+			//if there has been an error during login do the following
+			else{
+				echo "Error during login: An error has occured, please refresh the page to try again!";
+			}
+			http_response_code(200);
+		}
+		//close the connection
+		mysqli_close($conn);
+		
+	}
+	else {
+		http_response_code(403);
+		echo "Error during login: There was a problem with your submission, please try again!";
+	}
+		
+?>
